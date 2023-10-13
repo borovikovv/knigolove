4349fa9dd06fb475b96aef6c4a596d6b75d46cb7
Oleksii Borovykov <borovikovvaleksey@gmail.com>
2023-10-13 16:23:08 +0300

    connection to qulite3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { BooksModule } from './books/books.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), BooksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: [],
+    synchronize: true,
+  }),
+  ConfigModule.forRoot(),
+    BooksModule,
+    UsersModule
+  ],
 })
+
 export class AppModule {}
