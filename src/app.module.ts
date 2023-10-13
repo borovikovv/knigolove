da627ae5df2b76261781a969bad47c35e97f1d2f
Oleksii Borovykov <borovikovvaleksey@gmail.com>
2023-10-13 16:52:25 +0300

    book and user enteties

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+import { Book } from './books/book.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
     type: 'sqlite',
     database: 'db.sqlite',
-    entities: [],
+    entities: [User, Book ],
     synchronize: true,
   }),
   ConfigModule.forRoot(),
