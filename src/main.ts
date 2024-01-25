3b80f6c0ef4d6739aa7420618562b89dee1250df
Oleksii Borovykov <borovikovvaleksey@gmail.com>
2024-01-25 18:30:47 +0200

    added loger

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['asdfg'],
-  }));
+  app.use(morgan());
+  app.use(
+    cookieSession({
+      keys: ['asdfg'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-    })
-  )
+    }),
+  );
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 8000);
 }
 void bootstrap();
