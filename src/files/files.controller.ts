c39a249b76c09383e0551b78b66c4d9353d9d0d3
Oleksii Borovykov <borovikovvaleksey@gmail.com>
2024-01-26 16:27:02 +0200

    upload file to s3

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,14 +3,26 @@ import {
   Post,
   UploadedFile,
   UseInterceptors,
+  ParseFilePipe,
+  MaxFileSizeValidator,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesService } from './files.service';
 
 @Controller('file')
 export class FilesController {
+  constructor(private filesServise: FilesService) {}
+
   @Post('/upload')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new MaxFileSizeValidator({ maxSize: 1000000 })],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    await this.filesServise.upload(file.originalname, file.buffer);
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,7 +1,25 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class FilesService {
+  constructor(private configService: ConfigService) {}
+
+  private readonly s3Client = new S3Client({
+    region: this.configService.getOrThrow('AWS_S3_REGION'),
+  });
+
+  async upload(fileName: string, file: Buffer) {
+    await this.s3Client.send(
+      new PutObjectCommand({
+        Bucket: this.configService.getOrThrow('AWS_BUCKET_NAME'),
+        Key: fileName,
+        Body: file,
+      }),
+    );
+  }
+
   splitFileName(name: string) {
     return name.split('.').at(1);
   }
